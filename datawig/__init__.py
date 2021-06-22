5dc177793ac6b6ddf4d13dd56b23d1dbf57d859a
Felix Biessmann <biessman@amazon.com>
2021-06-22 13:22:14 +0200

    Cleaned up repo: - deleted docs and superfluous tests - updated quickstart examples - added example notebook

--- datawig/__init__.py
+++
@@ -1,7 +0,0 @@
-# makes the column encoders available as e.g. `from datawig import CategoricalEncoder`
-from .column_encoders import CategoricalEncoder, BowEncoder, NumericalEncoder, SequentialEncoder
-from .mxnet_input_symbols import BowFeaturizer, LSTMFeaturizer, NumericalFeaturizer, EmbeddingFeaturizer
-from .simple_imputer import SimpleImputer
-from .imputer import Imputer
-
-name = "datawig"
--- docs/source/contributing.rst
+++
@@ -1,6 +0,0 @@
-Contributing to DataWig
-=======================
-
-Please see `how to contribute`_.
-
-.. _`how to contribute`: https://github.com/awslabs/datawig/blob/master/CONTRIBUTING.md
